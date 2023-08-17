cd42272242da28d809d9760c620f1491f8a7abf5
Quattro <quattromusicofficial@gmail.com>
2023-08-17 13:32:52 +0200

    Improved math library

--- math.h
+++ math.h
@@ -48,7 +48,7 @@ inline float trunc(float x){
 #define PI      3.1415926535f
 #define E       2.7182818284f
 #define SQRT2   1.4142135623f
-#define SQRT3   1.73205080756f
+#define SQRT3   1.7320508075f
 
 inline float _sin_internal(float x){
     float q = 8 * x - 16 * x * x;
@@ -77,87 +77,80 @@ struct vec2{
     union{
         float ptr[2];
         struct {float x, y;};
-        __m128 v;
     };
 };
-inline vec2 operator +(vec2 a, vec2 b)  {vec2 res;  res.v = _mm_add_ps(a.v, b.v);  return res;}
-inline vec2 operator -(vec2 a, vec2 b)  {vec2 res;  res.v = _mm_sub_ps(a.v, b.v);  return res;}
-inline vec2 operator *(vec2 a, float s) {vec2 res;  res.v = _mm_mul_ps(a.v, _mm_set1_ps(s));  return res;}
-inline vec2 operator /(vec2 a, float s) {vec2 res;  res.v = _mm_div_ps(a.v, _mm_set1_ps(s));  return res;}
-inline vec2 operator -(vec2 a)          {vec2 res;  res.v = _mm_sub_ps(_mm_setzero_ps(), a.v);  return res;}
+inline vec2 operator +(vec2 a, vec2 b)  {return {a.x + b.x, a.y + b.y};}
+inline vec2 operator -(vec2 a, vec2 b)  {return {a.x - b.x, a.y - b.y};}
+inline vec2 operator *(vec2 a, float s) {return {a.x * s, a.y * s};}
+inline vec2 operator /(vec2 a, float s) {return {a.x / s, a.y / s};}
+inline vec2 operator -(vec2 a)          {return {-a.x, -a.y};}
 inline vec2 operator +=(vec2& a, const vec2& b) {a = a + b;  return a;}
 inline vec2 operator -=(vec2& a, const vec2& b) {a = a - b;  return a;}
-inline u8 operator ==(vec2 a, vec2 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) == 0b1111;}
-inline u8 operator !=(vec2 a, vec2 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) != 0b1111;}
+inline bool operator ==(vec2 a, vec2 b) {return (a.x == b.x) && (a.y == b.y);}
+inline bool operator !=(vec2 a, vec2 b) {return !(a == b);}
 inline vec2 rotate(vec2 v, float angle){
     float cos_value = cos(angle);
     float sin_value = sin(angle);
     
     vec2 res;
-    res.v = _mm_mul_ps(_mm_setr_ps(v.x, -v.y, v.x, v.y), _mm_setr_ps(cos_value, sin_value, sin_value, cos_value));
-    res.v = _mm_hadd_ps(res.v, _mm_set1_ps(0));
+    res.x = v.x * cos_value - v.y * sin_value;
+    res.y = v.x * sin_value + v.y * cos_value;
     return res;
 }
 inline float length(vec2 v){
-    vec2 res;
-    res.v = _mm_dp_ps(v.v, v.v, 0b00110001);
-    res.v = _mm_sqrt_ss(res.v);
-    return _mm_cvtss_f32(res.v);
+    return sqrt(v.x * v.x + v.y * v.y);
 }
 inline vec2 normalize(vec2 v){
-    vec2 res;
     float len = length(v);
-    res.v = _mm_div_ps(v.v, _mm_set1_ps(len));
-    return res;
+    return {v.x / len, v.y / len};
 }
-inline vec2 round(vec2 v) {vec2 res;  res.v = _mm_round_ps(v.v, _MM_FROUND_TO_NEAREST_INT);  return res;}
-inline vec2 floor(vec2 v) {vec2 res;  res.v = _mm_floor_ps(v.v);  return res;}
-inline vec2 ceil(vec2 v)  {vec2 res;  res.v = _mm_ceil_ps(v.v);  return res;}
-// inline vec2 trunc(vec2 v)  {return {math_trunc(v.x), math_trunc(v.y)};}
+inline vec2 round(vec2 v) {return {round(v.x), round(v.y)};}
+inline vec2 floor(vec2 v) {return {floor(v.x), floor(v.y)};}
+inline vec2 ceil(vec2 v)  {return {ceil(v.x), ceil(v.y)};}
+inline vec2 trunc(vec2 v) {return {trunc(v.x), trunc(v.y)};}
 
-//PERF(cogno): we can make this fast like other prints (look at first.h)
-inline void printsl(vec2 v) {printf("(%.3f, %.3f)", v.x, v.y);}
+inline void printsl(vec2 v) {fast_print("(%.3f, %.3f)", v.x, v.y);}
 
 struct vec3{
     union{
         float ptr[3];
         struct {float x, y, z;};
-        __m128 v;
     };
 };
-inline vec3 operator +(vec3 a, vec3 b)  {vec3 res; res.v = _mm_add_ps(a.v, b.v); return res;}
-inline vec3 operator -(vec3 a, vec3 b)  {vec3 res; res.v = _mm_sub_ps(a.v, b.v); return res;}
-inline vec3 operator *(vec3 a, float s) {vec3 res; res.v = _mm_mul_ps(a.v, _mm_set1_ps(s)); return res;}
-inline vec3 operator /(vec3 a, float s) {vec3 res; res.v = _mm_div_ps(a.v, _mm_set1_ps(s)); return res;}
-inline vec3 operator -(vec3 a)  {vec3 res;  res.v = _mm_sub_ps(_mm_setzero_ps(), a.v);  return res;}
+inline vec3 operator +(vec3 a, vec3 b)  {return {a.x + b.x, a.y + b.y, a.z + b.z};}
+inline vec3 operator -(vec3 a, vec3 b)  {return {a.x - b.x, a.y - b.y, a.z - b.z};}
+inline vec3 operator *(vec3 a, float s) {return {a.x * s, a.y * s, a.z * s};}
+inline vec3 operator /(vec3 a, float s) {return {a.x / s, a.y / s, a.z / s};}
+inline vec3 operator -(vec3 a)  {return {-a.x, -a.y, -a.z};}
 inline vec3 operator +=(vec3& a, const vec3& b) {a = a + b;  return a;}
 inline vec3 operator -=(vec3& a, const vec3& b) {a = a - b;  return a;}
-inline u8 operator ==(vec3 a, vec3 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) == 0b1111;}
-inline u8 operator !=(vec3 a, vec3 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) != 0b1111;}
+inline bool operator ==(vec3 a, vec3 b) {return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);}
+inline bool operator !=(vec3 a, vec3 b) {return !(a == b);}
 inline float length(vec3 v){
-    vec3 res;
-    res.v = _mm_dp_ps(v.v, v.v, 0b01110001);
-    res.v = _mm_sqrt_ss(res.v);
-    return _mm_cvtss_f32(res.v);
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 inline vec3 normalize(vec3 v){
-    vec3 res;
     float len = length(v);
-    res.v = _mm_div_ps(v.v, _mm_set1_ps(len));
-    return res;
+    return {v.x / len, v.y / len, v.z / len};
 }
-inline vec3 round(vec3 v) {vec3 res;  res.v = _mm_round_ps(v.v, _MM_FROUND_TO_NEAREST_INT);  return res;}
-inline vec3 floor(vec3 v) {vec3 res;  res.v = _mm_floor_ps(v.v);  return res;}
-inline vec3 ceil(vec3 v)  {vec3 res;  res.v = _mm_ceil_ps(v.v);  return res;}
-// inline vec3 trunc(vec3 v)  {return {math_trunc(v.x), math_trunc(v.y), math_trunc(v.z)};}
+inline vec3 round(vec3 v) {return {round(v.x), round(v.y), round(v.z)};}
+inline vec3 floor(vec3 v) {return {floor(v.x), floor(v.y), floor(v.z)};}
+inline vec3 ceil(vec3 v)  {return {ceil(v.x),  ceil(v.y),  ceil(v.z)};}
+inline vec3 trunc(vec3 v) {return {trunc(v.x), trunc(v.y), trunc(v.z)};}
 
-//PERF(cogno): we can make this fast like other prints (look at first.h)
-inline void printsl(vec3 v) {printf("(%.3f, %.3f, %.3f)", v.x, v.y, v.z);}
+inline void printsl(vec3 v) {fast_print("(%.3f, %.3f, %.3f)", v.x, v.y, v.z);}
 
 struct vec4{
     union{
         float ptr[4];
         struct {float x, y, z, w;};
+        __m128 v;
+    };
+};
+struct col{
+    union{
+        float ptr[4];
+        struct {float r, g, b, a;};
         __m128 v;
     };
 };
@@ -168,8 +161,8 @@ inline vec4 operator /(vec4 a, float s) {vec4 res; res.v = _mm_div_ps(a.v, _mm_s
 inline vec4 operator -(vec4 a)  {vec4 res;  res.v = _mm_sub_ps(_mm_setzero_ps(), a.v);  return res;}
 inline vec4 operator +=(vec4& a, const vec4& b) {a = a + b;  return a;}
 inline vec4 operator -=(vec4& a, const vec4& b) {a = a - b;  return a;}
-inline u8 operator ==(vec4 a, vec4 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) == 0b1111;}
-inline u8 operator !=(vec4 a, vec4 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) != 0b1111;}
+inline bool operator ==(vec4 a, vec4 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) == 0b1111;}
+inline bool operator !=(vec4 a, vec4 b) {return _mm_movemask_ps(_mm_cmpeq_ps(a.v, b.v)) != 0b1111;}
 inline float length(vec4 v){
     vec4 res;
     res.v = _mm_dp_ps(v.v, v.v, 0b11110001);
@@ -185,18 +178,10 @@ inline vec4 normalize(vec4 v){
 inline vec4 round(vec4 v) {vec4 res;  res.v = _mm_round_ps(v.v, _MM_FROUND_TO_NEAREST_INT);  return res;}
 inline vec4 floor(vec4 v) {vec4 res;  res.v = _mm_floor_ps(v.v);  return res;}
 inline vec4 ceil(vec4 v)  {vec4 res;  res.v = _mm_ceil_ps(v.v);  return res;}
-// inline vec4 trunc(vec4 v)  {...}
+inline vec4 trunc(vec4 v) {return {trunc(v.x), trunc(v.y), trunc(v.z), trunc(v.w)};}
 
-//PERF(cogno): we can make this fast like other prints (look at first.h)
-inline void printsl(vec4 v) {printf("(%.3f, %.3f, %.3f, %.3f)", v.x, v.y, v.z, v.w);}
+inline void printsl(vec4 v) {fast_print("(%.3f, %.3f, %.3f, %.3f)", v.x, v.y, v.z, v.w);}
 
-struct col{
-    union{
-        float ptr[4];
-        struct {float r, g, b, a;};
-        __m128 v;
-    };
-};
 inline col normalize(col c) {col res; res.v = _mm_div_ps(c.v, _mm_set1_ps(255)); return res;}
 
 struct mat4{
@@ -246,9 +231,40 @@ inline mat4 ortho(float left, float right, float bottom, float top, float z_near
     return res;
 }
 
-//PERF(cogno): we can make this fast like other prints (look at first.h)
-inline void printsl(mat4 m) {printf("|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n", m.m11, m.m12, m.m13, m.m14, m.m21, m.m22, m.m23, m.m24, m.m31, m.m32, m.m33, m.m34, m.m41, m.m42, m.m43, m.m44);}
+inline void printsl(mat4 m) {fast_print("|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n|%.3f %.3f %.3f %.3f|\n", m.m11, m.m12, m.m13, m.m14, m.m21, m.m22, m.m23, m.m24, m.m31, m.m32, m.m33, m.m34, m.m41, m.m42, m.m43, m.m44);}
 inline mat4 transpose(mat4 m) {_MM_TRANSPOSE4_PS(m.r1, m.r2, m.r3, m.r4);  return m;}
+inline mat4 operator +(mat4 m1, mat4 m2){
+    mat4 res;
+    res.r1 = _mm_add_ps(m1.r1, m2.r1);
+    res.r2 = _mm_add_ps(m1.r2, m1.r2);
+    res.r3 = _mm_add_ps(m1.r3, m1.r3);
+    res.r4 = _mm_add_ps(m1.r4, m1.r4);
+    return res;
+}
+inline mat4 operator -(mat4 m1, mat4 m2){
+    mat4 res;
+    res.r1 = _mm_sub_ps(m1.r1, m2.r1);
+    res.r2 = _mm_sub_ps(m1.r2, m1.r2);
+    res.r3 = _mm_sub_ps(m1.r3, m1.r3);
+    res.r4 = _mm_sub_ps(m1.r4, m1.r4);
+    return res;
+}
+inline mat4 operator *(mat4 m1, float s){
+    mat4 res;
+    res.r1 = _mm_mul_ps(m1.r1, _mm_set1_ps(s));
+    res.r2 = _mm_mul_ps(m1.r2, _mm_set1_ps(s));
+    res.r3 = _mm_mul_ps(m1.r3, _mm_set1_ps(s));
+    res.r4 = _mm_mul_ps(m1.r4, _mm_set1_ps(s));
+    return res;
+}
+inline mat4 operator /(mat4 m1, float s){
+    mat4 res;
+    res.r1 = _mm_div_ps(m1.r1, _mm_set1_ps(s));
+    res.r2 = _mm_div_ps(m1.r2, _mm_set1_ps(s));
+    res.r3 = _mm_div_ps(m1.r3, _mm_set1_ps(s));
+    res.r4 = _mm_div_ps(m1.r4, _mm_set1_ps(s));
+    return res;
+}
 inline mat4 operator *(mat4 m1, mat4 m2){
     mat4 temp = m2;
     _MM_TRANSPOSE4_PS(temp.r1, temp.r2, temp.r3, temp.r4);
@@ -258,14 +274,6 @@ inline mat4 operator *(mat4 m1, mat4 m2){
     res.r2 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(m1.r2, temp.r1, 0b11110001), _mm_dp_ps(m1.r2, temp.r2, 0b11110010)), _mm_or_ps(_mm_dp_ps(m1.r2, temp.r3, 0b11110100), _mm_dp_ps(m1.r2, temp.r4, 0b11111000)));
     res.r3 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(m1.r3, temp.r1, 0b11110001), _mm_dp_ps(m1.r3, temp.r2, 0b11110010)), _mm_or_ps(_mm_dp_ps(m1.r3, temp.r3, 0b11110100), _mm_dp_ps(m1.r3, temp.r4, 0b11111000)));
     res.r4 = _mm_or_ps(_mm_or_ps(_mm_dp_ps(m1.r4, temp.r1, 0b11110001), _mm_dp_ps(m1.r4, temp.r2, 0b11110010)), _mm_or_ps(_mm_dp_ps(m1.r4, temp.r3, 0b11110100), _mm_dp_ps(m1.r4, temp.r4, 0b11111000)));
-    return res;
-}
-inline mat4 operator +(mat4 m1, mat4 m2){
-    mat4 res;
-    res.r1 = _mm_add_ps(m1.r1, m2.r1);
-    res.r2 = _mm_add_ps(m1.r2, m1.r2);
-    res.r3 = _mm_add_ps(m1.r3, m1.r3);
-    res.r4 = _mm_add_ps(m1.r4, m1.r4);
     return res;
 }
 
