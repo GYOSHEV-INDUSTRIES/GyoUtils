4135807fdf527d33b9475e6f3a95dc1f798832a6
Cogno-Marco <marco.cognolato.98@gmail.com>
2023-10-01 14:06:19 +0200

    BENCHMARK_MANY_INPUTS macro

--- simple_benchmark.h
+++ simple_benchmark.h
@@ -87,7 +87,77 @@ do { \
     print_benchmark_times(min_cycles, total_cycles / count, max_cycles); \
 } while(0)
 
+// TODO(cogno): test the same func over and over for 10s, reset timer every time you get a new min
 
+/*
+Macro to repeatedly test a function n times with different inputs. Will record min, avg and max 
+execution time for each run and display it at the end of the whole test.
+Simply call BENCHMARK_VOID_MANY_INPUTS with the number of repetitions per test,
+the function to test and the inputs to each test (1 input per test)
+Only works with functions that return void and have 1 input.
+If you function returns something you can use BENCHMARK_MANY_INPUTS instead.
+Functions with multiple inputs are NOT supported.
+
+Example:
+BENCHMARK_VOID_MANY_INPUTS(10000, func_to_test, "a", "a string", "a longer string", "a very very long string");
+
+*/
+
+#define GET_FIRST(PAR, ...) PAR
+#define HAS_PARAMETER(...) GET_FIRST(placeholder, ##__VA_ARGS__, 0)
+
+#define BENCHMARK_VOID_MANY_INPUTS(count, func_name, ...) \
+do { \
+    decltype(MSVC_BUG(GET_FIRST, (__VA_ARGS__))) tests_inputs[] = {__VA_ARGS__}; \
+    const char* tests_inputs_names[] = {STRINGIFY(__VA_ARGS__)}; \
+    const int TESTS_INPUTS_COUNT = NUM_ARGS(__VA_ARGS__); \
+    u64 tests_mins[TESTS_INPUTS_COUNT] = {}; \
+    u64 tests_maxs[TESTS_INPUTS_COUNT] = {}; \
+    u64 tests_sums[TESTS_INPUTS_COUNT] = {}; \
+    for(int test_index = 0; test_index < TESTS_INPUTS_COUNT; test_index++) { \
+        for(int rep = 0; rep < count; rep++) { \
+            u64 start = read_cpu_timer(); \
+            func_name(tests_inputs[test_index]); \
+            u64 current_cycles = read_cpu_timer() - start; \
+            if(rep == 0 || current_cycles < tests_mins[test_index]) tests_mins[test_index] = current_cycles; \
+            if(current_cycles > tests_maxs[test_index]) tests_maxs[test_index] = current_cycles; \
+            tests_sums[test_index] += current_cycles; \
+        } \
+        print("completed testing with input %", tests_inputs_names[test_index]); \
+    } \
+    print("function '%' tested x% times", STRING_JOIN( STRING_JOIN( STRING_JOIN(#func_name, "("), #__VA_ARGS__  ) , ")" ), count); \
+    for(int i = 0; i < TESTS_INPUTS_COUNT; i++) { \
+        print("input '%': min=%, avg=%, max=% cycles", tests_inputs_names[i], tests_mins[i], tests_sums[i] / count, tests_maxs[i]); \
+    } \
+} while (0)
+
+// we use volatile so the code doesn't get optimized away
+#define BENCHMARK_MANY_INPUTS(count, func_name, ...) \
+do { \
+    decltype(MSVC_BUG(GET_FIRST, (__VA_ARGS__))) tests_inputs[] = {__VA_ARGS__}; \
+    const char* tests_inputs_names[] = {STRINGIFY(__VA_ARGS__)}; \
+    const int TESTS_INPUTS_COUNT = NUM_ARGS(__VA_ARGS__); \
+    u64 tests_mins[TESTS_INPUTS_COUNT] = {}; \
+    u64 tests_maxs[TESTS_INPUTS_COUNT] = {}; \
+    u64 tests_sums[TESTS_INPUTS_COUNT] = {}; \
+    for(int test_index = 0; test_index < TESTS_INPUTS_COUNT; test_index++) { \
+        for(int rep = 0; rep < count; rep++) { \
+            u64 start = read_cpu_timer(); \
+            volatile auto temp = func_name(tests_inputs[test_index]); \
+            u64 current_cycles = read_cpu_timer() - start; \
+            if(rep == 0 || current_cycles < tests_mins[test_index]) tests_mins[test_index] = current_cycles; \
+            if(current_cycles > tests_maxs[test_index]) tests_maxs[test_index] = current_cycles; \
+            tests_sums[test_index] += current_cycles; \
+        } \
+        print("completed testing with input %", tests_inputs_names[test_index]); \
+    } \
+    print("function '%' tested x% times", STRING_JOIN( STRING_JOIN( STRING_JOIN(#func_name, "("), #__VA_ARGS__  ) , ")" ), count); \
+    for(int i = 0; i < TESTS_INPUTS_COUNT; i++) { \
+        print("input '%': min=%, avg=%, max=% cycles", tests_inputs_names[i], tests_mins[i], tests_sums[i] / count, tests_maxs[i]); \
+    } \
+} while (0)
+
+// TODO(cogno): deprecated, delete
 #define BENCHMARK_FUNC(func_name, ...) \
 do { \
     BENCHMARK_WITH_COUNT(10, func_name, __VA_ARGS__); \
@@ -98,6 +168,7 @@ do { \
     BENCHMARK_WITH_COUNT(1000000, func_name, __VA_ARGS__); \
 } while(0)
 
+// TODO(cogno): deprecated, delete
 #define BENCHMARK_VOID_FUNC(func_name, ...) \
 do { \
     BENCHMARK_VOID_WITH_COUNT(10, func_name, __VA_ARGS__); \
