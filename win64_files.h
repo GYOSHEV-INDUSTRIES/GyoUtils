a50656ffcfc71a2194383fa5257a59516b6fdf17
Cogno-Marco <marco.cognolato.98@gmail.com>
2023-10-14 15:40:43 +0200

    Added get_only_files_in_dir in win64_files.h to know which folders are in a directory

--- win64_files.h
+++ win64_files.h
@@ -3,7 +3,8 @@
 /*
 In this file:
 functions to simplify working with windows' file system.
-- get_only_files_in_dir(...) to know which files only are in a dir
+- get_only_files_in_dir(...) to know which files only are in a directory
+- get_only_folders_in_dir(...) to know which folders only are in a directory
 - get_drive_names(...) to know which drives you have in your pc
 */
 
@@ -63,7 +64,56 @@ bool get_only_files_in_dir(str folder_path, Array<str>* filenames) {
     return true;
 }
 
-// TODO(cogno): get_only_folders_in_dir 
+bool get_only_folders_in_dir(str folder_path, Array<str>* folders) {
+    StrBuilder builder = make_str_builder();
+    defer(free(builder.ptr));
+    str_builder_append(&builder, folder_path);
+    str_builder_append(&builder, "\\*");
+    str_builder_append_raw(&builder, (u8)0);
+    
+    WIN32_FIND_DATA win_file_data = {};
+    HANDLE handle = FindFirstFile((char*)builder.ptr, &win_file_data);
+    defer(FindClose(handle));
+    if(handle == INVALID_HANDLE_VALUE) {
+        print("no initial file found");
+        return false;
+    }
+    
+    str_builder_remove_last_bytes(&builder, 2);
+    StrBuilder copy = str_builder_copy(&builder);
+    defer(free(copy.ptr));
+    
+    do {
+        // reset str builder into the initial folder path
+        copy.size = builder.size;
+        str_builder_append(&copy, win_file_data.cFileName);
+        str_builder_append_raw(&copy, (u8)0);
+        
+        // get info if it's a file or not
+        auto result = GetFileAttributes((const char*)copy.ptr);
+        if((int)result < 0) {
+            print("ERROR: invalid file attributes");
+            return false;
+        }
+        
+        if(result & FILE_ATTRIBUTE_DIRECTORY) {
+            // NOTE(cogno): windows is so fucking stupid it adds '.' and '..' as folders, we need to filter them out
+            str folder_name = str_new_alloc(win_file_data.cFileName); // else it doesn't live outside this function
+            if(str_matches(folder_name, "."))  continue;
+            if(str_matches(folder_name, "..")) continue;
+            array_append(folders, folder_name);
+        }
+        
+    } while(FindNextFile(handle, &win_file_data));
+    
+    auto err = GetLastError();
+    if(err != ERROR_NO_MORE_FILES) {
+        print("error: %", (int)err);
+        return false;
+    }
+    
+    return true;
+}
 
 // NOTE(cogno): each name will be terminated with ":\", so you'll see "C:\" etc.
 bool get_drive_names(Array<str>* drive_names) {
