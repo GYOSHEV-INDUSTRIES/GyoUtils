ce1832347ae86084b552da014ccae61681635a30
Cogno Marco <marco.cognolato.98@gmail.com>
2024-10-19 19:18:54 +0200

    ARRAY: ptr version of functions

--- array.h
+++ array.h
@@ -18,29 +18,94 @@ In this file:
 #define GYO_ARRAY_DEFAULT_SIZE 8
 
 template <typename T>
+void print_as_array(T* ptr, int array_size) {
+    if(ptr == NULL) return printsl("(null array)");
+    printsl('[');
+    for(int i = 0; i < array_size; i++) {
+        if (i != 0) printsl(',');
+        printsl(ptr[i]);
+    }
+    printsl(']');
+}
+
+template <typename T>
+void array_insert(T* ptr, int array_size, T to_insert, int index) {
+    ASSERT(ptr != NULL, "invalid buffer given (was NULL)");
+    if(!ASSERT_BOUNDS(index, 0, array_size + 1)) return;
+    
+    //move every data from index to end forward by 1
+    for(s32 i = array_size; i > index; i--) {
+        ptr[i] = ptr[i-1];
+    }
+    
+    ptr[index] = to_insert; //add new data
+}
+
+template <typename T>
+void array_remove_at(T* ptr, int array_size, int index) {
+    ASSERT(ptr != NULL, "invalid buffer given (was NULL)");
+    if(!ASSERT_BOUNDS(index, 0, array_size)) return;
+
+    //move every data from index to end back by 1
+    for(s32 i = index; i < array_size - 1; i++) {
+        ptr[i] = ptr[i + 1];
+    }
+    
+    //then clear last space with zeros (to avoid keeping invalid memory)
+    memset(ptr + array_size - 1, 0, sizeof(T));
+}
+
+
+// so you can use this as a stack (push=append)
+template<typename T>
+T array_pop(T* ptr, int array_size) {
+    ASSERT(array_size > 0, "cannot pop from an empty stack/array");
+    T element = ptr[array_size - 1];
+    memset(ptr + array_size - 1, 0, sizeof(T));
+    return element;
+}
+
+// so you can use this as a queue (queue=append)
+template<typename T>
+T array_dequeue(T* ptr, int array_size) {
+    ASSERT(array_size > 0, "cannot dequeue from an empty queue/array");
+    T element = ptr[0];
+    array_remove_at(ptr, array_size, 0);
+    return element;
+}
+
+template<typename T>
+T array_get_data(T* ptr, int array_size, int index) {
+    ASSERT_BOUNDS(index, 0, array_size);
+    return ptr[index];
+}
+
+template<typename T>
+void array_set(T* ptr, int array_size, int index, T value) {
+    ASSERT_BOUNDS(index, 0, array_size);
+    ptr[index] = value;
+}
+
+template<typename T>
+T* array_get_ptr(T* ptr, int array_size, int index) {
+    ASSERT_BOUNDS(index, 0, array_size);
+    return &ptr[index];
+}
+
+
+
+
+template <typename T>
 struct Array {
     s32 size = 0;
-    s32 reserved_size = 0;
+    s32 reserved_size = 0; // TODO(cogno): pass to s64/u64, an array of 4gb of data is kind of bad...
     T* ptr = NULL;
     Allocator alloc;
     T& operator[](s32 i) { ASSERT_BOUNDS(i, 0, size); return ptr[i]; }
 };
 
-// TODO(cogno): pass to s64/u64, an array of 4gb of data is kind of bad...
 
-template <typename T>
-void printsl_custom(Array<T> arr) {
-    if (arr.ptr == NULL) {
-        printsl("(null array)");
-        return;
-    }
-    printsl("[");
-    For(arr) {
-        if (it_index != 0) printsl(",");
-        printsl(it);
-    }
-    printsl("]");
-}
+template <typename T> void printsl_custom(Array<T> arr) { print_as_array(arr.ptr, arr.size); }
 
 // uses the custom given allocator
 template<typename T>
@@ -97,16 +162,7 @@ void array_reserve(Array<T>* array, s32 to_add) {
 template<typename T>
 void array_insert(Array<T>* array, T data, s32 index) {
     array_reserve(array, 1);
-    if(!ASSERT_BOUNDS(index, 0, array->size + 1)) return;
-    
-    //move every data from index to end forward by 1
-    for(s32 i = array->size; i > index; i--) {
-        array->ptr[i] = array->ptr[i-1];
-    }
-    
-    //add new data
-    array->ptr[index] = data;
-    array->size++;
+    array_insert(array->ptr, array->size++, data, index);
 }
 
 template<typename T>
@@ -115,55 +171,16 @@ void array_append(Array<T>* array, T data) {
     array->ptr[array->size++] = data;
 }
 
-template<typename T>
-void array_remove_at(Array<T>* array, s32 index) {
-    if(!ASSERT_BOUNDS(index, 0, array->size)) return;
-    
-    //move every data from index to end back by 1
-    for(s32 i = index; i < array->size - 1; i++) {
-        ASSERT(i >= 0 && i + 1 < array->size, "out of memory access");
-        array->ptr[i] = array->ptr[i + 1];
-    }
-    
-    //then clear last space with zeros (to avoid keeping invalid memory)
-    memset(array->ptr + array->size - 1, 0, sizeof(T));
-    array->size--;
-}
+template<typename T> void array_remove_at(Array<T>* array, s32 index) { array_remove_at(array->ptr, array->size--, index); }
 
 // so you can use this as a stack (push=append)
-template<typename T>
-T array_pop(Array<T>* array) {
-    ASSERT(array->size > 0, "cannot pop from an empty stack/array");
-    T element = array->ptr[array->size - 1];
-    array_remove_at(array, array->size - 1);
-    return element;
-}
+template<typename T> T array_pop(Array<T>* array) { return array_pop(array->ptr, array->size--); }
 
 // so you can use this as a queue (queue=append)
-template<typename T>
-T array_dequeue(Array<T>* array) {
-    ASSERT(array->size > 0, "cannot dequeue from an empty queue/array");
-    T element = array->ptr[0];
-    array_remove_at(array, 0);
-    return element;
-}
+template<typename T> T array_dequeue(Array<T>* array) { return array_dequeue(array->ptr, array->size--); }
 
 // NOTE(cogno): most of the times you can simply use the operator overload, so doing array[0] = 10; or auto temp = array[15];, but if you have the pointer to the array (instead of the array) then the operator overload will not work (because you'll be accessing the pointer!) so these functions can be used instead (or you can take a reference to the array instead of a pointer)
-template<typename T>
-T array_get_data(Array<T>* array, s32 index) {
-    ASSERT_BOUNDS(index, 0, array->size);
-    return array->ptr[index];
-}
-
-template<typename T>
-void array_set(Array<T>* array, s32 index, T value) {
-    ASSERT_BOUNDS(index, 0, array->size);
-    array->ptr[index] = value;
-}
-
-template<typename T>
-T* array_get_ptr(Array<T>* array, s32 index) {
-    ASSERT_BOUNDS(index, 0, array->size);
-    return &array->ptr[index];
-}
+template<typename T> T array_get_data(Array<T>* array, s32 index) { return array_get_data(array->ptr, array->size, index); }
+template<typename T> void array_set(Array<T>* array, s32 index, T value) { return array_set(array->ptr, array->size, index, value); }
+template<typename T> T* array_get_ptr(Array<T>* array, s32 index) { return array_get_ptr(array->ptr, array->size, index); }
 
