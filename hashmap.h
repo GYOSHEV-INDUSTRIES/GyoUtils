50c917df28253c5b5e37e0067d82f7d43c63ea7e
Cogno Marco <marco.cognolato.98@gmail.com>
2024-10-31 13:13:44 +0100

    HASHMAP: improvements
    > removed elements array and HashPair struct, now the data is inside the matrix/solver itself, leading to less memory allocations and faster code for single accesses (slower for chain searches, we'll deal with those later on by reallocating the entire hashmap)

--- hashmap.h
+++ hashmap.h
@@ -15,21 +15,13 @@ In this file:
     #include "array.h"
 #endif
 
-template<typename T, typename U>
-struct HashPair {
-    T key;
-    U value;
-};
-
-template<typename T, typename U> void printsl_custom(HashPair<T, U> pair) {
-    printsl("{k=%, v=%}", pair.key, pair.value);
-}
-
 // API(cogno): it would be cool to have 0 as invalid index, but then the stack has 1 extra element which is wack when you iterate. We could say "each index add/remove 1" as if the array is 1-based instead of 0-based, i don't know...
 #define MAP_INVALID_INDEX -1
 
+template<typename T, typename U>
 struct Finder {
-    int pair_index        = MAP_INVALID_INDEX;
+    T key;
+    U value;
     int next_finder_index = MAP_INVALID_INDEX;
 };
 
@@ -63,15 +55,11 @@ u64 hash_default<str>(str* string, int size) {
 
 template<typename T, typename U>
 struct HashMap {
-    // since we want to iterate over elements using our For macro we hold them in a stack and use a second special array for hashes.
-    Array<HashPair<T, U>> elements; // API(cogno): if we inline the array into ptr + size + reserved_size we can directly iterate over elements with our For macro which might be cooler
-    // TODO(cogno): this ^ is a bad idea because removing elements is TERRIBLE. Instead on focusing on having ALL elements quickly, focus on removing them, then having all of them can take as much as we want, it doesn't matter. Alternatively have the whole list with a flag that tells you if it's valid and when you iterate over them skip useless ones.
-
     // API(cogno): a fixed size allocator would be good too but where do you hold the Bump allocator data?
-    Finder* matrix_ptr; // if you don't have conflicts this fills up (and does NOT resize)
+    Finder<T, U>* matrix_ptr; // if you don't have conflicts this fills up (and does NOT resize)
     int matrix_size;
     
-    Array<Finder> solver; // if you have conflicts this fills up (and *does* resize)
+    Array<Finder<T, U>> solver; // if you have conflicts this fills up (and *does* resize)
     // target: fill as much matrix with as little solver as possible
     
     // PERF(cogno): if we want to make this even faster we might SOA intead of AOS. Instead of holding an array of pair/next, we hold an array of pair and an array of next (such that with an index you can get both in different arrays)
@@ -90,16 +78,13 @@ HashMap<T, U> make_hashmap(int size, Allocator alloc) {
     map.alloc = alloc;
     
     // make the fixed array which we use for hashing
-    void* matrix_space = mem_alloc(alloc, size * sizeof(Finder));
-    map.matrix_ptr = (Finder*)matrix_space;
+    void* matrix_space = mem_alloc(alloc, size * sizeof(Finder<T, U>));
+    map.matrix_ptr = (Finder<T, U>*)matrix_space;
     map.matrix_size = size;
     for(int i = 0; i < size; i++) map.matrix_ptr[i] = {}; // PERF(cogno): if we can have MAP_INVALID_INDEX == 0 then this becomes useless (but we also need to force the array with zeros...)
     
-    // make space to hold the actual key-value pairs
-    map.elements = make_array<HashPair<T, U>>(size, alloc);
-    
     // and finally make space to solve hashing conflicts
-    map.solver = make_array<Finder>(size, alloc);
+    map.solver = make_array<Finder<T, U>>(size, alloc);
     // API(cogno): we should rename size for array/hashmap to say element count and for mem_alloc/allocator to say byte count
     return map;
 }
@@ -122,27 +107,25 @@ HashMap<T, U> make_hashmap(int size) { return make_hashmap<T, U>(size, default_a
 
 template<typename T, typename U>
 void map_insert(HashMap<T, U>* map, T key, U value) {
-    HashPair<T, U> p = {key, value};
-    
     u64 hash = hash_default(&key, sizeof(key));
     
     int matrix_index = hash % map->matrix_size;
     ASSERT_BOUNDS(matrix_index, 0, map->matrix_size);
-    Finder* current_finder = &map->matrix_ptr[matrix_index];
-    if(current_finder->pair_index == MAP_INVALID_INDEX) {
-        array_append(&map->elements, p);
+    Finder<T,U>* current_finder = &map->matrix_ptr[matrix_index];
+    if(current_finder->next_finder_index == MAP_INVALID_INDEX) {
+        // this spot is free, let's occupy it!
         // simple case, no hash conflict, replace finder
-        Finder new_finder = {};
-        new_finder.pair_index = map->elements.size - 1;
-        map->matrix_ptr[matrix_index] = new_finder;
+        current_finder->key = key;
+        current_finder->value = value;
+        array_append(&map->solver, {}); // start an empty collision chain for successive insertions
+        current_finder->next_finder_index = map->solver.size - 1; // the one we just added
     } else {
         // complex case, either we have the key and we need to replace the value, or we have to use solver to set a new key
         while(true) {
-            int possible_location = current_finder->pair_index;
             // API(cogno): what if the struct doesn't have operator equals?
-            if(map->elements[possible_location].key == key) {
+            if(current_finder->key == key) {
                 // key found, replace value
-                map->elements[possible_location].value = value;
+                current_finder->value = value;
                 return;
             }
             
@@ -152,11 +135,10 @@ void map_insert(HashMap<T, U>* map, T key, U value) {
         }
         
         // value is new, add to collision queue in the solver
-        array_append(&map->elements, p);
-        Finder new_finder = {};
-        new_finder.pair_index = map->elements.size - 1;
-        array_append(&map->solver, new_finder);
-        current_finder->next_finder_index = map->solver.size - 1; // keep the linked list intact
+        current_finder->key = key;
+        current_finder->value = value;
+        array_append(&map->solver, {}); // extend collision chain by 1
+        current_finder->next_finder_index = map->solver.size - 1; // the one we just added
     }
 }
 
@@ -166,17 +148,14 @@ bool map_find(HashMap<T, U>* map, T key, U* out_value) {
     u64 hash = hash_default(&key, sizeof(key));
     int index = hash % map->matrix_size;
     ASSERT_BOUNDS(index, 0, map->matrix_size);
-    Finder* current_finder = &map->matrix_ptr[index];
+    Finder<T, U>* current_finder = &map->matrix_ptr[index];
+    if(current_finder->next_finder_index == MAP_INVALID_INDEX) return false; // no elements ever set
     
     while (true) {
-        int possible_location = current_finder->pair_index;
-        if(possible_location == MAP_INVALID_INDEX) break; // no elements ever set
-        
         // location valid, check if the key is the same
         // API(cogno): what if the struct doesn't have operator equals?
-        auto pair = map->elements[possible_location];
-        if(pair.key == key) {
-            *out_value = pair.value;
+        if(current_finder->key == key) {
+            *out_value = current_finder->value;
             return true;
         }
         
@@ -185,7 +164,7 @@ bool map_find(HashMap<T, U>* map, T key, U* out_value) {
         current_finder = &map->solver[next_index];
     }
     
-    return false;
+    return false; // the entire chain doesn't contain this key
 }
 
 template<typename T, typename U>
